92c72bd796633d68d9253adb20c2515aad5d0e7c
amiam <amiambagheri@gmail.com>
2024-11-03 00:36:16 +0330

    chore: add Shared Class library

---
+++ SharedClass/FileAddress.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SharedClass;
+
+public class FileAddress
+{
+    public string _transactionFilePath = @"D:\c# project\POSsimulator-OTPgenerator\Transactions";
+    public string _transactionFileName = "Transactions.json";
+    public string _cardsFilePath = @"D:\c# project\POSsimulator-OTPgenerator\Cards";
+    public string _cardsFileName = "Cards.json";
+}
